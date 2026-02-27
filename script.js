611016042ae64e3e48789af8202a6df518df253f
NicholasLupo <nicholaslupo14@gmail.com>
2026-02-27 14:10:27 +0100

    add calculation of the minor ticket price

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ console.log(age)
 let price = km * 0.21
 let priceFixed = price.toFixed(2);
 console.log(priceFixed + '€')
+
+// Calcolo biglietto per minori
+if (age <= 17) {
+
+    let price = km * 0.21
+    let sales = price - ((price * 20) / 100)
+    let salesFixed = sales.toFixed(2);
+    console.log(salesFixed + '€')
+
+}
