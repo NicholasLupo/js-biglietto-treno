25491f624441376366b30e7faf0ac5be1c30322c
NicholasLupo <nicholaslupo14@gmail.com>
2026-02-27 13:53:07 +0100

    add calculation of the basic ticket price

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let km = prompt('Inserisci il numero di chilometri che vuoi percorrere');
 console.log(km)
 const age = prompt('Inserisci la tua età');
 console.log(age)
+
+// Calcolo biglietto base
+let price = km * 0.21
+console.log(price + '€')
